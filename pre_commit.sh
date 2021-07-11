507a57d4f8aa6e5ca4a4bd111436304fa7d2b56d
John-Gee <john.ettedgui@gmail.com>
2021-07-10 20:06:09 -0700

    Version css and js files to prevent bad caching

---
+++ pre_commit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+function addCSUM() {
+    extension="$1"
+
+    grep -o -P -e "\".*\.$extension.*?\"" index.html | while read -r line
+    do
+        if [[ $line == *"http"* ]]
+        then
+            continue
+        fi
+
+        line=${line:1:-1}
+        cleanLine="${line%\?*}"
+        xxcsum=`xxh32sum "$cleanLine"`
+        csum="${xxcsum:0:8}"
+        sed -i "s|\"$line\"|\"$cleanLine?c=$csum\"|" index.html
+        echo $line $csum
+
+    done
+}
+
+addCSUM js
+addCSUM css
